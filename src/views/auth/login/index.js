c9e580927cd580ff07f9806ebde4e6208d3fdd41
kisonkim <npolly7@gmail.com>
2021-11-30 23:39:30 +0900

    [feat]버튼 수정 및 배너적용 시도

--- src/views/auth/login/index.js
+++ src/views/auth/login/index.js
@@ -15,7 +15,6 @@ const Login = () =>{
 
   const onSubmitValid = async(data) => {
     let {email, password} = data
-    console.log(email,password)
     // let inquiry = textAreaValue.current.value;
     
     // setLoading(true)
@@ -45,6 +44,7 @@ const Login = () =>{
   return(
     <FullBackWrapper background={background}>
       <WhiteBox>
+        {/* <div className="banner">국내 최초 플랫폼 촬영 대행 서비스</div> */}
         <div className="title">BRIPHY</div>
         <div className="sub-title">로그인</div>
         <form onSubmit={handleSubmit(onSubmitValid)}>
@@ -89,12 +89,23 @@ const WhiteBox = styled.div`
   color:white;
   width:400px;
   transform:translate3d(-100%,0,0);
-  .title{
+
+  .banner {
+    color:white;
+    margin-bottom:1rem;
+    background-color:#FE5A1D;
+    text-align:center;
+    line-height:2;
+    font-size:1.2rem;
+    font-weight:600;
+    border-radius:5px;
+  }
+  .title {
     font-size:4rem;
     font-weight:600;
     text-align: center;
   }
-  .sub-title{
+  .sub-title {
     margin:1.25rem 0;
     font-size:1.25rem;
     text-align: center;
@@ -108,11 +119,11 @@ const InputWrapper = styled.div`
     outline:none;
     border:none;
     background:transparent;
-    border-bottom: ${props=>props.hasError?'1px solid tomato':'1px solid white'};
+    border-bottom: ${props=>props.hasError?'1px solid #FE5A1D':'1px solid white'};
     width:100%;
     color:white;
     &::placeholder{
-      color:${props=>props.hasError?'tomato':'#ffffff94'};
+      color:${props=>props.hasError?'#FE5A1D':'#ffffff94'};
     }
   }
 `;
@@ -140,7 +151,7 @@ const PolicyField = styled.div`
     margin: 0 10px 0 0;
     transform: translate3d(0, -7%, 0);
     background: url(${checkbox}) left center no-repeat; 
-    border: 0.5px solid ${props=>props.hasError?'tomato':'none'};  
+    border: 0.5px solid ${props=>props.hasError?'#FE5A1D':'none'};  
     vertical-align: middle; 
     transition-duration: .2s; 
   }
@@ -148,13 +159,14 @@ const PolicyField = styled.div`
 
 const SubmitButton = styled.input`
   border: none;
-  border-radius:1rem;
+  border-radius:5px;
   margin-top: 12px;
   background-color: #FE5A1D;
   color: white;
   text-align: center;
   padding: 8px 0px;
   font-weight: 600;
+  font-size:1.2rem;
   width:100%;
   cursor:pointer;
   opacity: ${props=>props.disabled ? "0.5" : "1"};
